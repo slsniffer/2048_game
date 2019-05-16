a83d6e6e841bf407f935321c19cb22efc610d574
slsniffer <sl.sniffer@gmail.com>
2019-05-16 08:04:33 -0400

    - split files, ignore temp folders.

---
+++ TwentyFortyEight.ConsoleUI/ColorSchema.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeamSL.TwentyFortyEight.ConsoleUI
+{
+    public class ColorSchema
+    {
+        private readonly Dictionary<ushort, ConsoleColor> _colors;
+
+        public ColorSchema()
+        {
+            _colors = new Dictionary<ushort, ConsoleColor>();
+            _colors.Add(0, ConsoleColor.DarkGray);
+            _colors.Add(2, ConsoleColor.White);
+            _colors.Add(4, ConsoleColor.Yellow);
+            _colors.Add(8, ConsoleColor.DarkYellow);
+            _colors.Add(16, ConsoleColor.Red);
+            _colors.Add(32, ConsoleColor.DarkRed);
+            _colors.Add(64, ConsoleColor.Magenta);
+            _colors.Add(128, ConsoleColor.Cyan);
+            _colors.Add(256, ConsoleColor.DarkCyan);
+            _colors.Add(512, ConsoleColor.Blue);
+            _colors.Add(1024, ConsoleColor.DarkBlue);
+            _colors.Add(2048, ConsoleColor.Green);
+        }
+
+        public ConsoleColor GetColor(ushort value)
+        {
+            if (value > 2048)
+                return _colors[2048];
+
+            if (!_colors.ContainsKey(value))
+                throw new ArgumentOutOfRangeException(nameof(value));
+
+            return _colors[value];
+        }
+    }
+}
---
+++ TwentyFortyEight.ConsoleUI/Drawer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TeamSL.TwentyFortyEight.Engine;
+
+namespace TeamSL.TwentyFortyEight.ConsoleUI
+{
+    public class Drawer
+    {
+        private readonly ColorSchema _colorSchema;
+
+        public Drawer(ColorSchema colorSchema)
+        {
+            _colorSchema = colorSchema;
+        }
+
+        public void Draw(Game game)
+        {
+            Console.Clear();
+            Console.WriteLine();
+
+            var defaultColor = Console.ForegroundColor;
+
+            for (int rowIndex = 0; rowIndex < game.Board.Cells.GetLength(0); rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < game.Board.Cells.GetLength(1); columnIndex++)
+                {
+                    var boardCell = game.Board.Cells[rowIndex, columnIndex];
+
+                    Console.ForegroundColor = _colorSchema.GetColor(boardCell.Value);
+                    Console.Write($"{boardCell.Value,6}");
+                    Console.ForegroundColor = defaultColor;
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Score: {0}", game.Score);
+            Console.WriteLine("Use arrows for movements, Backspace to undo last movement...");
+        }
+    }
+}
